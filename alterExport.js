5930b74fa37142a081ebedcaeabb6c8c7f07c164
Ocean <s@ocean.lol>
2022-04-22 08:02:46 +0100

    colon problems

--- alterExport.js
+++ alterExport.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import canvas from 'canvas';
 import { diffWords } from 'diff';
 
+const exportImages = false;
+
 const width = 678;
 const height = 874;
 
@@ -23,13 +25,17 @@ const height = 874;
         let up = cards.find(e => e.name == c.name+'+'); //find upgraded version of card
 
         //create image of card next to its upgrade
-        let canv = canvas.createCanvas(width * (up == undefined ? 1 : 2), height); //double-width canvas if there is an upgrade
-        let ctx = canv.getContext('2d');
+        if (exportImages) {
+            let canv = canvas.createCanvas(width * (up == undefined ? 1 : 2), height); //double-width canvas if there is an upgrade
+            let ctx = canv.getContext('2d');
+        }
         let cardPath = `${c.color.slice(0,10)}-${c.name.replaceAll(' ', '').replaceAll(':', '-').replaceAll('\'', '').replaceAll('?', '')}`;
         let imgPath = `docs/${c.mod == '' ? 'slay-the-spire' : c.mod}/card-images/${cardPath}`;
-        ctx.drawImage(await canvas.loadImage(imgPath+'.png'), 0, 0);
+        if (exportImages)
+            ctx.drawImage(await canvas.loadImage(imgPath+'.png'), 0, 0);
         if (up != undefined) {
-            ctx.drawImage(await canvas.loadImage(imgPath+'Plus.png'), width, 0);
+            if (exportImages)
+                ctx.drawImage(await canvas.loadImage(imgPath+'Plus.png'), width, 0);
 
             //update card to include numbers from upgrade
             if (c.cost != up.cost) c.cost = `${c.cost} (${up.cost})`;
@@ -41,10 +47,10 @@ const height = 874;
                 if (!word.hasOwnProperty('added'))
                     c.description += word.value;
                 else if (word.added)
-                    c.description += ` (${word.value.replace('\n', '')})${word.value.includes('\n') ? '\n' : ''}`;
+                    c.description += ` (${word.value.includes(':') ? ' ' : ''}${word.value.replace('\n', '')}${word.value.includes(':') ? ' ' : ''})${word.value.includes('\n') ? '\n' : ''}`;
                 else if (word.removed) {
                     if (!diff.hasOwnProperty(i+1) || !diff[i+1].added)
-                        c.description += `${(word.value.endsWith('\n') ? word.value.replace('\n', '') : word.value).replace('.', '').replace(' ', '')} (not ${word.value.replace('\n', '').replace('.', '').replace(' ', '')})${word.value.includes('.') ? '.' : ''}${word.value.endsWith(' ') ? ' ' : ''}${word.value.endsWith('\n') ? '\n' : ''}`;
+                        c.description += `${(word.value.endsWith('\n') ? word.value.replace('\n', '') : word.value).replace('.', '').replace(' ', '')} (not ${word.value.replace('\n', '').replace('.', '').replace(' ', '')}${word.value.includes(':') ? ' ' : ''})${word.value.includes('.') ? '.' : ''}${word.value.endsWith(' ') ? ' ' : ''}${word.value.endsWith('\n') ? '\n' : ''}`;
                     else
                         c.description += word.value;
                 }
@@ -52,10 +58,12 @@ const height = 874;
             c.description = c.description.replaceAll('([E]', '( [E]');
             console.log('\n'+c.description);
         }
-        //save image
-        let out = fs.createWriteStream(`docs/altered/img/cards/${cardPath}.png`);
-        canv.createPNGStream().pipe(out);
-        out.on('finish', () => finish());
+        if (exportImages) {
+            //save image
+            let out = fs.createWriteStream(`docs/altered/img/cards/${cardPath}.png`);
+            canv.createPNGStream().pipe(out);
+            out.on('finish', () => finish());
+        } else finish()
 
         newCards.push(c);
         await finished;
