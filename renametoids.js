15c21cd159a80d56b3f35942d9111fbba36910bc
Ocean <s@ocean.lol>
2023-04-13 22:39:53 +0100

    names to ids part 2

--- renametoids.js
+++ renametoids.js
@@ -3,15 +3,15 @@ import fs from 'fs';
 let data = JSON.parse(fs.readFileSync('./docs/data.json'));
 
 for (let c of data.cards) {
-    let oldPath = `docs/${c.mod}/${c.color.slice(0,10)}-${c.name.replaceAll(' ', '').replaceAll(':', '-').replaceAll('\'', '').replaceAll('?', '').replaceAll('"', '').replaceAll('/', '')}.png`;
-    let newPath = `docs/${c.mod}/${c.id.replaceAll(' ', '').replaceAll(':', '-').replaceAll('\'', '').replaceAll('?', '').replaceAll('"', '').replaceAll('/', '')}.png`;
+    let oldPath = `./docs/${c.mod}/cards/${c.color.slice(0,10)}-${c.name.replaceAll(' ', '').replaceAll(':', '-').replaceAll('\'', '').replaceAll('?', '').replaceAll('"', '').replaceAll('/', '')}.png`;
+    let newPath = `./docs/${c.mod}/cards/${c.id.replaceAll(' ', '').replaceAll(':', '-').replaceAll('\'', '').replaceAll('?', '').replaceAll('"', '').replaceAll('/', '')}.png`;
     if (fs.existsSync(oldPath))
-        fs.mvSync(oldPath, newPath);
+        fs.renameSync(oldPath, newPath);
 }
 
 for (let p of data.potions) {
-    let oldPath = `docs/${p.mod}/${p.name.replaceAll(' ', '')}.png`;
-    let newPath = `docs/${p.mod}/${p.id.replaceAll(' ', '')}.png`;
+    let oldPath = `./docs/${p.mod}/potions/${p.name.replaceAll(' ', '')}.png`;
+    let newPath = `./docs/${p.mod}/potions/${p.id.replaceAll(' ', '').replaceAll(':', '-')}.png`;
     if (fs.existsSync(oldPath))
-        fs.mvSync(oldPath, newPath);
+        fs.renameSync(oldPath, newPath);
 }
